c25e3cbcb8f35be2cc71ae9a99dd939861cc64ed
Mehmet Arikan <mehmetarikaan@outlook.com>
2022-08-26 14:07:08 +0300

    create data model

---
+++ App/AnimalsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimalsApp.swift
+//  Animals
+//
+//  Created by Mehmet Arıkan on 26.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct AnimalsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ App/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Animals
+//
+//  Created by Mehmet Arıkan on 26.08.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
