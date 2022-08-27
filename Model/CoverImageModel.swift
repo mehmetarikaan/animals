803edc3c9d2996befa70f4802b4180099b13f1f9
Mehmet Arikan <mehmetarikaan@outlook.com>
2022-08-27 14:03:11 +0300

    created model view

---
+++ Model/CoverImageModel.swift
@@ -0,0 +1,13 @@
+//
+//  CoverImageModel.swift
+//  Animals
+//
+//  Created by Mehmet Arıkan on 27.08.2022.
+//
+
+import SwiftUI
+
+struct CoverImage: Codable, Identifiable{
+    let id : Int
+    let name: String
+}
---
+++ View/CoverImageView.swift
@@ -0,0 +1,28 @@
+//
+//  CoverImageView.swift
+//  Animals
+//
+//  Created by Mehmet Arıkan on 27.08.2022.
+//
+
+import SwiftUI
+
+struct CoverImageView: View {
+    var body: some View {
+        TabView{
+            ForEach(/*@START_MENU_TOKEN@*/0 ..< 5/*@END_MENU_TOKEN@*/) { item in
+                Image("cover-lion")
+                    .resizable()
+                .scaledToFit()
+            }//: Loop
+        } //: Tab
+        .tabViewStyle(PageTabViewStyle())
+    }
+}
+
+struct CoverImageView_Previews: PreviewProvider {
+    static var previews: some View {
+        CoverImageView()
+            .previewLayout(.fixed(width: 400, height: 300))
+    }
+}
