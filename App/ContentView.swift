1899bf84aedd4a965386a218cb7a4d244a653e37
Mehmet Arikan <mehmetarikaan@outlook.com>
2022-08-28 17:13:31 +0300

    editing content view for detail view

--- App/ContentView.swift
+++ App/ContentView.swift
@@ -17,8 +17,10 @@ struct ContentView: View {
                     .listRowInsets(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0))
                     
                 ForEach(animals) { animal in
-                    AnimalListItemView(animal: animal)
-                }
+                    NavigationLink(destination: AnimalDetailView(animal: animal)){
+                        AnimalListItemView(animal: animal)
+                    }//: Link
+                }//: Loop
             }//: List
             .navigationBarTitle("Africa", displayMode: .large)
         }//: Navigation
