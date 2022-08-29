c24a76df2f3696913537a015381f251366377867
Mehmet Arikan <mehmetarikaan@outlook.com>
2022-08-29 17:09:13 +0300

    created inset mapview

---
+++ View/InsetMapView.swift
@@ -0,0 +1,46 @@
+//
+//  InsetMapView.swift
+//  Animals
+//
+//  Created by Mehmet Arıkan on 28.08.2022.
+//
+
+import SwiftUI
+import MapKit
+
+struct InsetMapView: View {
+    @State private var region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 6.600286, longitude: 16.4377599), span: MKCoordinateSpan(latitudeDelta: 60.0, longitudeDelta: 60.0))
+    var body: some View {
+        Map(coordinateRegion: $region)
+            .overlay(
+                NavigationLink(destination: MapView()){
+                    HStack{
+                        Image(systemName: "mappin.circle")
+                            .foregroundColor(.white)
+                            .imageScale(.large)
+                        Text("Locations")
+                            .foregroundColor(.accentColor)
+                            .fontWeight(.bold)
+                    }//: Hstack
+                    .padding(.horizontal, 14)
+                    .padding(.vertical, 10)
+                        .background(
+                            Color.black
+                            .opacity(0.4)
+                            .cornerRadius(8)
+                        )
+                }//: NavigationLink
+                    .padding(12), alignment: .topTrailing
+            )
+            .frame(height: 256)
+            .cornerRadius(25)
+    }
+}
+
+struct InsetMapView_Previews: PreviewProvider {
+    static var previews: some View {
+        InsetMapView()
+            .previewLayout(.sizeThatFits)
+            .padding()
+    }
+}
