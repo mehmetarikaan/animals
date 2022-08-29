669029faff08ff6492e15fb3255123291d97276e
Mehmet Arikan <mehmetarikaan@outlook.com>
2022-08-29 17:10:26 +0300

    created video model

---
+++ Model/VideoModel.swift
@@ -0,0 +1,14 @@
+//
+//  VideoModel.swift
+//  Animals
+//
+//  Created by Mehmet Arıkan on 29.08.2022.
+//
+
+import SwiftUI
+
+struct Video: Codable, Identifiable{
+    let id: String
+    let name: String
+    let headline: String
+}
