23d65cbd795fd527b1b49748cfa0ac50c3e4b502
Mehmet Arikan <mehmetarikaan@outlook.com>
2022-08-29 17:09:36 +0300

    created inset fact view

---
+++ View/InsetFactView.swift
@@ -0,0 +1,32 @@
+//
+//  InsetFactView.swift
+//  Animals
+//
+//  Created by Mehmet Arıkan on 28.08.2022.
+//
+
+import SwiftUI
+
+struct InsetFactView: View {
+    let animal: Animal
+    var body: some View {
+        GroupBox{
+            TabView{
+                ForEach(animal.fact, id: \.self) { item in
+                    Text(item)
+                }//:Loop
+            }//TABS
+            .tabViewStyle(PageTabViewStyle())
+            .frame(minHeight: 148, idealHeight: 168, maxHeight: 180)
+        }//: BOX
+    }
+}
+
+struct InsetFactView_Previews: PreviewProvider {
+    static let animals: [Animal] = Bundle.main.decode("animals.json")
+    static var previews: some View {
+        InsetFactView(animal: animals[0])
+            .previewLayout(.sizeThatFits)
+            .padding()
+    }
+}
